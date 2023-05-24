4d1fb9d4eab879addf73f54ac5d0849e6feccadb
KwameSei <nathanielosei92@gmail.com>
2023-05-24 10:42:05 +0000

    Solving the island perimeter puzzle

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" Solving the island perimeter puzzle """
+
+
+def island_perimeter(grid):
+    """ Finding the perimeter of an island """
+    row_index = 0
+    col_index = 0
+    perimeter = 0
+
+    for row_index, row in enumerate(grid):
+        for col_index, col in enumerate(row):
+            if col == 1:
+                # perimeter += 4
+
+                if row_index > 0 and row_index < (len(grid) - 1):
+                    if grid[row_index + 1][col_index] == 0:
+                        perimeter += 1
+
+                    if grid[row_index - 1][col_index] == 0:
+                        perimeter += 1
+
+                    if row[col_index + 1] == 0:
+                        perimeter += 1
+
+                    if row[col_index - 1] == 0:
+                        perimeter += 1
+
+    return perimeter
---
+++ 0x09-island_perimeter/README.md
@@ -0,0 +1,8 @@
+<h1>Island Perimeter</h1>
+<p>You are given a map in form of a two-dimensional integer grid where 1 represents land and 0 represents water.</p>
+
+<p>Grid cells are connected <strong>horizontally/vertically</strong> (not diagonally). The grid is completely surrounded by water, and there is exactly one island (i.e., one or more connected land cells).</p>
+
+<p>The island doesn't have "lakes" (water inside that isn't connected to the water around the island). One cell is a square with side length 1.</p>
+
+<p>The grid is rectangular, width and height don't exceed 100. Determine the perimeter of the island.</p>
